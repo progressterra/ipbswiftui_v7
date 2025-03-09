cffdf895156d17845cbc2d7613a6bee204003fad
ihomosum <ihomosum@gmail.com>
2025-03-10 00:39:02 +0300

    feat: ErrorAlertView

--- Sources/ipbswiftui_v7/Views/Components/StatusAlertView.swift
+++ Sources/ipbswiftui_v7/Views/Components/StatusAlertView.swift
@@ -69,3 +69,38 @@ public struct StatusAlertView: View {
         }
     }
 }
+
+
+public struct ErrorAlertView: View {
+    @Binding var status: String
+    let onDisappear: () -> ()
+    
+    private let bouncyAnimation = Animation.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.5)
+    
+    public init(status: Binding<String>,  onDisappear: @escaping () -> Void) {
+        self._status = status
+        self.onDisappear = onDisappear
+    }
+    
+    public var body: some View {
+        if status != "" {
+            Text(status)
+                .padding()
+                .foregroundStyle(Style.textButtonPrimary)
+                .font(Style.headline)
+                .background(Style.primary)
+                .cornerRadius(8)
+                .shadow(radius: 10)
+                .multilineTextAlignment(.center)
+                .transition(.scale.combined(with: .opacity).animation(bouncyAnimation))
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            self.status = ""
+                            onDisappear()
+                        }
+                    }
+                }
+        }
+    }
+}

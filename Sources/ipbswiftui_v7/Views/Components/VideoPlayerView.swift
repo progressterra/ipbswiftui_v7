8baa772528b2db1c80d476ede556590715810298
ihomosum <ihomosum@gmail.com>
2025-03-10 02:14:54 +0300

    debug: video

--- Sources/ipbswiftui_v7/Views/Components/VideoPlayerView.swift
+++ Sources/ipbswiftui_v7/Views/Components/VideoPlayerView.swift
@@ -4,7 +4,8 @@ import AVKit
 struct VideoPlayerView: View {
     let videoURL: URL
     @State private var player: AVPlayer?
-    //@Binding var isVideoPresented: Bool
+    @State private var isPlaying = false
+    @State private var showPlayButton = true
 
     var body: some View {
         ZStack {
@@ -12,32 +13,90 @@ struct VideoPlayerView: View {
             
             if let player = player {
                 VideoPlayer(player: player)
-                    .onAppear {
-                        player.play()
-                    }
-                    .onDisappear {
-                        player.pause()
+                    .onTapGesture {
+                        togglePlayPause()
                     }
             }
-            
-//            VStack {
-//                HStack {
-//                    Spacer()
-//                    Button(action: {
-//                        isVideoPresented = false
-//                    }) {
-//                        Image(systemName: "xmark.circle.fill")
-//                            .font(.system(size: 30))
-//                            .foregroundColor(.white)
-//                            .padding()
-//                    }
-//                }
-//                Spacer()
-//            }
+
+            if showPlayButton {
+                Button(action: {
+                    playVideo()
+                }) {
+                    Image(systemName: "play.circle.fill")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .shadow(radius: 10)
+                }
+                .transition(.opacity)
+                .animation(.easeInOut(duration: 1.0), value: showPlayButton)
+            }
         }
         .onAppear {
             player = AVPlayer(url: videoURL)
             player?.pause()
         }
     }
+
+    private func playVideo() {
+        player?.play()
+        isPlaying = true
+        withAnimation {
+            showPlayButton = false
+        }
+    }
+
+    private func togglePlayPause() {
+        if isPlaying {
+            player?.pause()
+            isPlaying = false
+            withAnimation {
+                showPlayButton = true
+            }
+        }
+    }
 }
+
+//import SwiftUI
+//import AVKit
+//
+//struct VideoPlayerView: View {
+//    let videoURL: URL
+//    @State private var player: AVPlayer?
+//    //@Binding var isVideoPresented: Bool
+//
+//    var body: some View {
+//        ZStack {
+//            Color.black.ignoresSafeArea()
+//            
+//            if let player = player {
+//                VideoPlayer(player: player)
+//                    .onAppear {
+//                        player.play()
+//                    }
+//                    .onDisappear {
+//                        player.pause()
+//                    }
+//            }
+//            
+////            VStack {
+////                HStack {
+////                    Spacer()
+////                    Button(action: {
+////                        isVideoPresented = false
+////                    }) {
+////                        Image(systemName: "xmark.circle.fill")
+////                            .font(.system(size: 30))
+////                            .foregroundColor(.white)
+////                            .padding()
+////                    }
+////                }
+////                Spacer()
+////            }
+//        }
+//        .onAppear {
+//            player = AVPlayer(url: videoURL)
+//            player?.pause()
+//        }
+//    }
+//}
